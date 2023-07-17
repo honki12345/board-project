c63d664b6cab9f22a8515c5e94bfb49bbb86c50d
honki12345 <iui4793@gmail.com>
2023-07-17 22:06:41 +0900

    #7 - 스프링 부트 프로젝트 시작 스프링 부트 프로젝트 초기화 들어간 초기 기술은:
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/me/honki12345/boardproject/BoardProjectApplication.java
@@ -0,0 +1,13 @@
+package me.honki12345.boardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardProjectApplication.class, args);
+    }
+
+}
